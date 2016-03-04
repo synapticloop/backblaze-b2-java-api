419f397621d2072f2178f14e201857efbfc30981
David Kocher <dkocher@iterate.ch>
2016-03-04 10:16:45 +0100

    Remove redundant #toString.

--- src/main/java/synapticloop/b2/Action.java
+++ src/main/java/synapticloop/b2/Action.java
@@ -11,17 +11,6 @@ package synapticloop.b2;
  * @author synapticloop
  */
 public enum Action {
-	HIDE("hide"),
-	UPLOAD("upload");
-
-	private final String actionType;
-
-	Action(String actionType) {
-		this.actionType = actionType;
-	}
-
-	@Override
-	public String toString() {
-		return(actionType);
-	}
+	hide,
+	upload
 }
