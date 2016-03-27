93d438546c010012f5d0ef24af5ea74c6443f377
David Kocher <dkocher@iterate.ch>
2016-03-27 21:43:31 +0200

    Parse Retry-After response header and add value in seconds to exception. Refer to https://www.backblaze.com/b2/docs/integration_checklist.html

--- src/test/java/synapticloop/b2/exception/B2ApiExceptionTest.java
+++ src/test/java/synapticloop/b2/exception/B2ApiExceptionTest.java
@@ -10,13 +10,13 @@ public class B2ApiExceptionTest {
 
 	@Test
 	public void testEmptyException() {
-		B2ApiException b2Exception = new B2ApiException("");
+		B2ApiException b2Exception = new B2ApiException("", new RuntimeException());
 		assertEquals("", b2Exception.getMessage());
 	}
 
 	@Test
 	public void testNullResponseException() {
-		B2ApiException b2Exception = new B2ApiException((String)null);
+		B2ApiException b2Exception = new B2ApiException(null, new RuntimeException());
 		assertEquals(null, b2Exception.getMessage());
 	}
 
@@ -27,7 +27,7 @@ public class B2ApiExceptionTest {
 				"      \"message\": \"unknown field in com.backblaze.modules.b2.data.FileNameAndId: accountId\",\n" + 
 				"      \"status\": 400\n" + 
 				"    }\n" + 
-				")");
+				")", new RuntimeException());
 		assertEquals(400, b2Exception.getStatus());
 		assertEquals("bad_json", b2Exception.getCode());
 		assertEquals("unknown field in com.backblaze.modules.b2.data.FileNameAndId: accountId", b2Exception.getMessage());
